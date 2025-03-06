ea5cd4b110fb4d9da9d7201b8ba0217c29c0ce39
mreed4 <78713317+mreed4@users.noreply.github.com>
2025-03-05 21:18:02 -0800

    Merge commit 'c6d6bbf9c074096e77da41e9371e7dcdf67fe105' as 'jamming'

---
+++ jamming/src/Components/App.jsx
@@ -0,0 +1,55 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+
+import { AppContext } from "./Wrappers/AppContext";
+import ScrollToTop from "./Wrappers/ScrollToTop";
+
+import Header from "./Header";
+import ItemLists from "./Results/ItemLists";
+import SearchPage from "./Search/SearchPage";
+import ResultsPage from "./Results/ResultsPage";
+import LoadingPage from "./LoadingPage";
+import AlbumDetailsPage from "./Albums/AlbumDetailsPage";
+import ArtistDetailsPage from "./Artists/ArtistDetailsPage";
+import Playlist from "./Playlist";
+
+const netlify = "/.netlify/functions";
+
+export default function App() {
+  const { searchResults, resultsArray } = useContext(AppContext);
+
+  async function getAccessToken() {
+    const response = await fetch(`${netlify}/requestAccessToken`);
+    const token = await response.json();
+    return token;
+  }
+
+  useEffect(() => {
+    getAccessToken();
+  }, []);
+
+  return (
+    <Router>
+      <ScrollToTop>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            {resultsArray.length > 0 ? (
+              <Route path="/results" element={<ResultsPage />}>
+                {resultsArray.reverse().map((key) => {
+                  return <Route key={key} path={key} element={<ItemLists items={searchResults[key].items} itemTypes={key} />} />;
+                })}
+              </Route>
+            ) : (
+              <Route path="/results/tracks" element={<LoadingPage />} /> // This is the route location of useNavigate() in SearchForm.jsx
+            )}
+            <Route path="/album/:artist/:name" element={<AlbumDetailsPage />} />
+            <Route path="/artist/:name" element={<ArtistDetailsPage />} />
+            <Route path="*" element={<h2>404</h2>} />
+          </Routes>
+        </main>
+      </ScrollToTop>
+    </Router>
+  );
+}
